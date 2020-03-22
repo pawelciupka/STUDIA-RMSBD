5c386681aeb7f9379565003d7f0b33568892cb9b
Maciej Majchrowski <maciek21197@gmail.com>
2020-03-22 16:54:41 +0100

    formula to add package by product names

--- zadanie 1/functions.sql
+++ zadanie 1/functions.sql
@@ -4,15 +4,68 @@
 --
 --------------------
 
-create or replace function quantityOfSelectedFoodType(food_name IN varchar2(50))
-RETURN number
-IS
+-- sprawdzenie ilości posiadanego danego typu jedzenia w bazie
+create or replace function quantityOfSelectedFoodType(food_name IN varchar)
+RETURN number IS
     food_type_count number;
-begin
-  select count(*) from food_type into 
+BEGIN
+select SUM(package.package_size * product.weight) into food_type_count 
+    from package
+    INNER JOIN  product
+    ON package.product_id = product.product_id
+    WHERE product.food_type = food_name
+
+return food_type_count;
+END;
+/
+-- wywołanie funkcji quantityOfSelectedFoodType
+declare
+    c varchar(20);
+    food_type product.food_type%TYPE := 'napoj';
+begin 
+    c := quantityOfSelectedFoodType(food_type);
+    dbms_output.put_line('W magazynie produktow typu: ' || food_type || ' jest: ' || c || ' kilogramow');
 end;
+/
 
 -- dodawanie do pólek rzeczy według nazy produktu, nie ID
+create or replace procedure add_Package_Of_Product
+(
+    nazwa_produktu IN product.name%type,
+    nazwa_opakowania IN package.name%type,
+    liczba_szt IN package.package_size%type,
+    data_waznosci IN package.expiration_date%type
+)
+is
+liczba_wystapien_nazwy number;
+id_produktu number := 0;
+brak_produktu EXCEPTION;
+begin
+    select product.product_id into id_produktu 
+    from product
+    where product.name = nazwa_produktu;
+liczba_wystapien_nazwy := sql%rowcount;
+if(id_produktu = 0) then 
+    raise brak_produktu;
+end if;
+INSERT INTO package(name, product_id, expiration_date, placement_id, package_size) 
+VALUES (
+    nazwa_opakowania,
+    id_produktu,
+    TO_DATE(data_waznosci, 'yyyy/mm/dd'),
+    1,
+    liczba_szt
+);
+dbms_output.put_line('dodano nowe opakownie do bazy');
+exception
+    when brak_produktu then
+        dbms_output.put_line('nie znaleziono produktu o nazwie: ' || nazwa_produktu);
+end add_Package_Of_Product; 
+
+set serveroutput on;
+begin
+    add_Package_Of_Product(nazwa_produktu=>'Ser gouda 1kg', nazwa_opakowania=>'trojpak plastikowy', liczba_szt=>3, data_waznosci=>'2020/04/15');
+end;
 
 -- wyciąganie rzeczy z konkretnej ilości, nie pojedynczo
 
--- zadanie 1/selectFormulas.sql
+++ zadanie 1/selectFormulas.sql
@@ -1,3 +1,14 @@
-select package.name, package.expiration_date, product.name, product.producent, product.name from package
+-- wyswietlanie wspolnej wagi dla wszystkich produktow danego typu w bazie
+select package.name, package.package_size, 
+        product.name, product.weight, 
+        product.producent, 
+        product.food_type, 
+        package.package_size * product.weight as laczna_waga from package
 INNER JOIN  product
     ON package.product_id = product.product_id
+WHERE product.food_type = 'napoj';
+
+select SUM(package.package_size * product.weight) as laczna_waga from package
+INNER JOIN  product
+    ON package.product_id = product.product_id
+WHERE product.food_type = 'napoj';

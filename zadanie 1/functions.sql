05ae8e909eeb10f46645af781087215ff50f8817
Maciej Majchrowski <maciek21197@gmail.com>
2020-03-22 18:39:33 +0100

    added 4th procedure

--- zadanie 1/functions.sql
+++ zadanie 1/functions.sql
@@ -62,12 +62,94 @@ exception
         dbms_output.put_line('nie znaleziono produktu o nazwie: ' || nazwa_produktu);
 end add_Package_Of_Product; 
 
+
+-- wyswietlenie w scipt outpu
 set serveroutput on;
 begin
     add_Package_Of_Product(nazwa_produktu=>'Ser gouda 1kg', nazwa_opakowania=>'trojpak plastikowy', liczba_szt=>3, data_waznosci=>'2020/04/15');
 end;
 
--- wyciąganie rzeczy z konkretnej ilości, nie pojedynczo
+-- wyciąganie jednej rzeczy z konkretnego opakowania o nazwie
+
+create or replace procedure take_one_item
+(
+    nazwa_opakowania in package.name%type,
+    info_w in transaction.info%type
+)
+is
+id_package package.package_id%type := 0;
+brak_opakowania EXCEPTION;
+begin
+    select package.package_id into id_package
+    from package
+    where package.name = nazwa_opakowania
+    fetch next 1 rows only;
+
+if(id_package = 0)then
+    raise brak_opakowania;
+end if;
+update package
+    set package_size=package_size - 1
+    where package.package_id=id_package;
+dbms_output.put_line('Wyjeto jeden produkt z opakowania: ' || nazwa_opakowania);
+INSERT INTO transaction(info, time_stamp, package_id) VALUES 
+(
+    info_w,
+    TO_DATE(CURRENT_DATE, 'yyyy/mm/dd'),
+    id_package
+);
+exception 
+    when brak_opakowania then
+        dbms_output.put_line('nie znaleziono opakowania o nazwie: ' || nazwa_opakowania);
+end take_one_item;
+
+-- wyswietlenie w scipt outpu
+set serveroutput on;
+begin
+    take_one_item(nazwa_opakowania=>'Dwupak plastikowy', info_w=>'pora na kanapki');
+end;
+
+-- wyciąganie konkretnej liczby rzeczy z opakowania o nazwie i sprawdzenie ilosci
+
+create or replace procedure take_x_packages
+(
+    nazwa_opakowania in package.name%type,
+    liczba_wyciaganych in package.package_size%type,
+    info_w in transaction.info%type
+)
+is
+id_package package.package_id%type := 0;
+brak_odpowiedniej_liczby EXCEPTION;
+begin
+    select package.package_id into id_package
+    from package
+    where package.name = nazwa_opakowania and package.package_size >= liczba_wyciaganych
+    fetch next 1 rows only;
+
+if(id_package = 0)then
+    raise brak_odpowiedniej_liczby;
+end if;
+update package
+    set package_size=(package_size-liczba_wyciaganych)
+    where package.package_id=id_package;
+dbms_output.put_line('Wyjeto ' || liczba_wyciaganych || ' opakowania: ' || nazwa_opakowania);
+INSERT INTO transaction(info, time_stamp, package_id, number_of_taken_items) VALUES 
+(
+    info_w,
+    TO_DATE(CURRENT_DATE, 'yyyy/mm/dd'),
+    id_package,
+    liczba_wyciaganych
+);
+exception 
+    when brak_odpowiedniej_liczby then
+        dbms_output.put_line('nie znaleziono opakowania o nazwie/nie ma tyle w magazynie: ' || nazwa_opakowania);
+end take_x_packages;
+
+-- wyswietlenie w scipt outpu
+set serveroutput on;
+begin
+    take_x_packages(nazwa_opakowania=>'Zgrzewka - 12 szt',liczba_wyciaganych=>4,info_w=>'pora na picie');
+end;
 
 -- agregowanie rzeczy o tej samej dacie ważności i produkcie
 

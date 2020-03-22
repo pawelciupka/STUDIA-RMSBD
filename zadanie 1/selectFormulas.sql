201dd1dd4bc6357daba9dc366eff8ac274ae9334
Maciej Majchrowski <maciek21197@gmail.com>
2020-03-22 20:27:35 +0100

    added one select

--- zadanie 1/selectFormulas.sql
+++ zadanie 1/selectFormulas.sql
@@ -12,3 +12,13 @@ select SUM(package.package_size * product.weight) as laczna_waga from package
 INNER JOIN  product
     ON package.product_id = product.product_id
 WHERE product.food_type = 'napoj';
+
+-- wybierz po nazwie nazwe polki z produktem o nizszej dacie przydatnosci
+select placement.info from placement
+inner join package 
+on placement.placement_id = package.placement_id    
+inner join product
+on package.product_id = product.product_id
+where product.name = 'Szynka drobiowa 100g'
+ORDER BY package.expiration_date asc
+FETCH FIRST 1 ROWS ONLY

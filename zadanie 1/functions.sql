f69f8a41f5c87ebfbcedcc562abe1995646e0a0d
Maciej Majchrowski <maciek21197@gmail.com>
2020-03-23 13:16:44 +0100

    hot fix

--- zadanie 1/functions.sql
+++ zadanie 1/functions.sql
@@ -1,10 +1,11 @@
 -- Paweł Ciupka 234048
--- Maciej Majchrowski 234
+-- Maciej Majchrowski 234088
 --
 --
 --------------------
 
 -- sprawdzenie ilości posiadanego danego typu jedzenia w bazie
+--!! wykonanie każdego z poleceń osobno działa, z jakiegoś powodu gdy uruchomi się cały skrypt to sqlserver sie gubi
 create or replace function quantityOfSelectedFoodType(food_name IN varchar)
 RETURN number IS
     food_type_count number;
@@ -13,7 +14,7 @@ select SUM(package.package_size * product.weight) into food_type_count
     from package
     INNER JOIN  product
     ON package.product_id = product.product_id
-    WHERE product.food_type = food_name
+    WHERE product.food_type = food_name;
 
 return food_type_count;
 END;
@@ -37,32 +38,35 @@ create or replace procedure add_Package_Of_Product
     data_waznosci IN package.expiration_date%type
 )
 is
-id_produktu number := 0;
+id_produktu product.product_id%type := 0;
 brak_produktu EXCEPTION;
 begin
     select product.product_id into id_produktu 
     from product
-    where product.name = nazwa_produktu;
+    where product.name = nazwa_produktu
     fetch next 1 rows only;
+dbms_output.put_line('znaleziono ' || id_produktu);
 INSERT INTO package(name, product_id, expiration_date, placement_id, package_size) 
 VALUES (
     nazwa_opakowania,
     id_produktu,
-    TO_DATE(data_waznosci, 'yyyy/mm/dd'),
+    data_waznosci,
     1,
     liczba_szt
 );
 dbms_output.put_line('dodano nowe opakownie do bazy');
-exception
-    when no_data_found then 
-        dbms_output.put_line('nie znaleziono produktu o nazwie: ' || nazwa_produktu);
 end add_Package_Of_Product; 
+/
 
 
--- wyswietlenie w scipt outpu
-set serveroutput on;
-begin
-    add_Package_Of_Product(nazwa_produktu=>'Ser gouda 1kg', nazwa_opakowania=>'trojpak plastikowy', liczba_szt=>3, data_waznosci=>'2020/04/15');
+declare
+    productName product.name%type := 'Ser gouda 1kg';
+    opakowanie package.name%type := 'trojpak plastikowy';
+begin 
+    add_Package_Of_Product(productName,
+                            opakowanie,
+                            3, 
+                            TO_DATE('2020/03/19', 'yyyy/mm/dd'));
 end;
 
 -- wyciąganie jednej rzeczy z konkretnego opakowania o nazwie
@@ -87,19 +91,20 @@ dbms_output.put_line('Wyjeto jeden produkt z opakowania: ' || nazwa_opakowania);
 INSERT INTO transaction(info, time_stamp, package_id) VALUES 
 (
     info_w,
-    TO_DATE(CURRENT_DATE, 'yyyy/mm/dd'),
+    CURRENT_DATE,
     id_package
 );
 exception 
     when no_data_found then 
         dbms_output.put_line('nie znaleziono opakowania o nazwie: ' || nazwa_opakowania);
 end take_one_item;
+/
 
 -- wyswietlenie w scipt outpu
-set serveroutput on;
 begin
     take_one_item(nazwa_opakowania=>'Dwupak plastikowy', info_w=>'pora na kanapki');
 end;
+/
 
 -- wyciąganie konkretnej liczby rzeczy z opakowania o nazwie i sprawdzenie ilosci
 
@@ -124,7 +129,7 @@ dbms_output.put_line('Wyjeto ' || liczba_wyciaganych || ' opakowania: ' || nazwa
 INSERT INTO transaction(info, time_stamp, package_id, number_of_taken_items) VALUES 
 (
     info_w,
-    TO_DATE(CURRENT_DATE, 'yyyy/mm/dd'),
+    CURRENT_DATE,
     id_package,
     liczba_wyciaganych
 );
@@ -132,14 +137,12 @@ exception
     when no_data_found then 
         dbms_output.put_line('nie znaleziono opakowania o nazwie/nie ma tyle w magazynie: ' || nazwa_opakowania);
 end take_x_packages;
+/
 
 -- wyswietlenie w scipt outpu
-set serveroutput on;
 begin
     take_x_packages(nazwa_opakowania=>'Zgrzewka - 12 szt',liczba_wyciaganych=>4,info_w=>'pora na picie');
 end;
-
--- agregowanie rzeczy o tej samej dacie ważności i produkcie
 
 -- szukaj rzecz na polkach o krótszym terminie przydatności
 create or replace function find_less_fresh_product
@@ -157,14 +160,21 @@ begin
     where product.name = product_name
     ORDER BY package.expiration_date asc
     FETCH FIRST 1 ROWS ONLY;
-
-exception
-  when no_data_found then
-    dbms_output.put_line('nie znaleziono produktu w magazynie: ' || product_name);
-
+dbms_output.put_line('znaleziono produktu w magazynie: ' || nazwa_polki);
 return nazwa_polki;
 end;
 /
+
+-- wywołanie funkcji find_less_fresh_product
+declare
+    c placement.info%type;
+    product_name product.name%type := 'Szynka drobiowa 100g';
+begin 
+    c := find_less_fresh_product(product_name);
+    dbms_output.put_line('W magazynie: ' || product_name || ' jest na : ' || c);
+end;
+/
+
 
 -- wywołanie funkcji find_less_fresh_product
 declare
@@ -243,7 +253,6 @@ begin
     EXCEPTION
         when placement_overload then
             dbms_output.put_line('polka nie zmiesci kolejnego opakowania, #polki:' || :new.placement_id);
-            rollback transaction
         when no_data_found then
             dbms_output.put_line('nie znaleziono odpowiedniech danych w bazie');
 end check_placement_capacity;
